25e65b09e7fcab7129811f47b80fd491463e60df
AbinashB <abinash.bswl@gmail.com>
2025-11-02 16:05:05 +0700

    remove old way for fetching data

--- 08currencyconverter/src/App.jsx
+++ 08currencyconverter/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import {InputBox} from './components'
-import useCurrencyInfo from "./hooks/userCurrencyInfo"
+import useCurrencyInfo from "./hooks/useCurrencyInfo"
 
 
 function App() {
